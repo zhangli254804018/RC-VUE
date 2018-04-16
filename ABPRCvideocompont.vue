b2561adf4e7462f70e301f308974ac395f90e93d
zhangli <254804018@qq.com>
2018-04-16 10:45:35 +0800

    新增啊 大佬们

---
+++ ABPRCvideocompont.vue
@@ -0,0 +1,310 @@
+<template>
+    <!-- 獨立出來一個播放視頻的組件 -->
+    <div class="ABPRC-Unit" :class="{'ABPRC-FullScreen':fullScreen }" @mouseover="onControl(true)" @mouseout="onControl(false)" >                        
+        <div class="ABPRC-Video" @click="onPlay" >
+            <div class="ABPRC-Container"></div>
+            <div class="ABPRC-info" v-if="dialogData.share">
+               <p class="title">{{dialogData.title || ''}}</p>
+               <p class="nick">{{dialogData.nick || ''}}</p>
+            </div>
+            <video id="ABPRCPlayer" :src="dialogData.path" crossorigin preload autoplay playsinline loop >
+                <!-- <source :src="dialogData.path" type="video/mp4" /> -->
+            </video>
+        </div>
+        <div class="ABPRC-Control" :class="[ Control ? 'fadeIn animated' : 'fadeOut animated']">
+            <div class="button ABPRC-Play" :class="{'ABPRC-Pause' : playing}"  @click="onPlay"></div>
+            <div class="progress-bar">
+                <div class="bar dark" :style="barTime.style"></div>
+                <div class="bar" :style="barLoad.style"></div>
+            </div>
+            <!-- <div class="button ABPRC-CommentShow"></div> -->
+            <div class="button ABPRC-FullScreen" @click="onFullScreen"></div>
+        </div>
+    </div>          
+</template>
+
+<script>
+import { browserPlatform } from "src/api/util";
+export default {
+  data() {
+      //传入的参数
+      // dialogData  =  {
+      //   "uid": 111,
+			// 	"nick": "fcsfdsf",
+			// 	"id": "1",
+			// 	"title": "test",
+			// 	"path": "https://ttvideo-cdnqn.52tt.com/o_1c4m5h7u099m11salku1t761jmoa.mp4",
+			// 	"status": "1",
+			// 	"cate": "1",
+      //   "score": 1
+      //   };        
+    return {
+      playing: false,
+      fullScreen: false,
+      Control:true,
+      barTime: {
+        style: {
+          width: 0
+        }
+      },
+      barLoad: {
+        style: {
+          width: 0
+        }
+      }
+    };
+  },
+  props:['dialogData'],
+  components: {},
+  methods: {
+    onControl(bool,time){
+      const vm = this
+       this.Control = bool;
+       time && setTimeout(() => {
+          vm.Control = false
+      }, time*5);
+    },
+    onPlay() {
+      const video = document.getElementById("ABPRCPlayer");
+      this.onControl(true,2000);
+      if (!this.playing) {
+        video.play();
+        this.playing = true;
+        this.swapVideo();
+      } else {
+        video.pause();
+        this.playing = false;
+      }
+    },
+    onFullScreen() {
+      this.fullScreen = !this.fullScreen;
+    },
+    swapVideo() {
+      var ABPInst = this;
+      var video = document.getElementById("ABPRCPlayer");
+
+      video.addEventListener("timeupdate", function() {
+        ABPInst.barTime.style.width =
+          video.currentTime / video.duration * 100 + "%";
+      });
+      video.addEventListener("ended", function() {
+        ABPInst.btnPlay.className = "button ABP-Play";
+        ABPInst.barTime.style.width = "0%";
+      });
+      video.addEventListener("progress", function() {
+        if (this.buffered != null) {
+          try {
+            var s = this.buffered.start(0);
+            var e = this.buffered.end(0);
+          } catch (err) {
+            return;
+          }
+          var dur = this.duration;
+          var perc = e / dur * 100;
+          ABPInst.barLoad.style.width = perc + "%";
+        }
+      });
+      video.addEventListener("loadedmetadata", function() {
+        if (this.buffered != null) {
+          try {
+            var s = this.buffered.start(0);
+            var e = this.buffered.end(0);
+          } catch (err) {
+            return;
+          }
+          var dur = this.duration;
+          var perc = e / dur * 100;
+          ABPInst.barLoad.style.width = perc + "%";
+        }
+      });
+
+      video.addEventListener("play", function() {
+        try {
+          var e = this.buffered.end(0);
+          var dur = this.duration;
+          var perc = e / dur * 100;
+          ABPInst.barLoad.style.width = perc + "%";
+        } catch (err) {}
+      });
+
+      var pbar = document.getElementsByClassName("progress-bar");
+      if (pbar.length <= 0) return;
+      ABPInst.barHitArea = pbar[0];
+      ABPInst.video = document.getElementById("ABPRCPlayer");
+      var dragging = false;
+
+      ABPInst.barHitArea.addEventListener("mousedown", function(e) {
+        dragging = true;
+      });
+      document.addEventListener("mouseup", function(e) {
+        dragging = false;
+      });
+      ABPInst.barHitArea.addEventListener("mouseup", function(e) {
+        dragging = false;
+        var newTime = (ABPInst.fullScreen ? e.pageX : e.layerX) / this.offsetWidth * ABPInst.video.duration;
+        ABPInst.video.currentTime = newTime;
+      });
+      ABPInst.barHitArea.addEventListener("mousemove", function(e) {
+        if (dragging) {
+          ABPInst.barTime.style.width = (ABPInst.fullScreen ? e.pageX : e.layerX) * 100 / this.offsetWidth + "%";
+        }
+      });
+    }
+  },
+  mounted: function() {
+    var vm = this;
+      setTimeout(() => {
+        vm.onPlay();
+        //蘋果瀏覽器依然為暫停自動播放
+        if(browserPlatform().mobile &&browserPlatform().ios ) vm.playing = false
+      }, 500);
+  },
+  computed: {
+    
+  }
+};
+</script>
+<style lang="less" scope>
+@import url("~assets/less/_mixins.less");
+//獨立出來一個組件啊大佬們
+.ABPRC-Unit {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  background: #000;
+}
+.ABPRC-Unit video {
+  width: 100%;
+  height: 100%;
+}
+.ABPRC-Unit .ABPRC-Control {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: #000;
+  opacity: 0.9;
+  border-top: 1px solid #33b5e5;
+  height: 40px;
+  -moz-user-select: -moz-none;
+  -khtml-user-select: none;
+  -webkit-user-select: none;
+  -o-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.ABPRC-Unit.ABPRC-FullScreen {
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: auto !important;
+  height: auto !important;
+  position: fixed;
+  z-index: 99999999;
+  background: #000;
+}
+
+.ABPRC-Unit .ABPRC-Video {
+  position: absolute;
+  overflow: hidden;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 12px;
+  background: #000;
+}
+
+.ABPRC-Unit .ABPRC-Video .ABPRC-Container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: #000;
+  opacity: 0;
+  z-index: 9;
+  overflow: hidden;
+  bottom: 40px;
+}
+
+.ABPRC-Unit .ABPRC-Video .ABPRC-info {
+  position: absolute;
+  bottom: 40px;
+  left: 20px;
+  p {
+    color: #fff;
+    &.title {
+      font-size: 16px;
+    }
+    &.nick {
+      font-size: 14px;
+    }
+  }
+}
+
+.ABPRC-Unit .ABPRC-Control:hover {
+  opacity: 1;
+}
+
+.ABPRC-Unit .ABPRC-Control .button {
+  position: absolute;
+  overflow: hidden;
+  background-color: #000;
+  width: 40px;
+  top: 0;
+  bottom: 0;
+  -webkit-transition: background-color 0.2s ease-in-out;
+  transition: background-color 0.2s ease-in-out;
+}
+
+.ABPRC-Unit .ABPRC-Control .button:hover {
+  background-color: #6cf;
+}
+
+.ABPRC-Unit .ABPRC-Control .ABPRC-Play {
+  background-image: url("@{img-url}play.png");
+  background-size: cover;
+  left: 0;
+}
+
+.ABPRC-Unit .ABPRC-Control .ABPRC-Play.ABPRC-Pause {
+  background-image: url("@{img-url}pause.png");
+}
+
+.ABPRC-Unit .ABPRC-Control .ABPRC-FullScreen {
+  background-image: url("@{img-url}fullscreen.png");
+  background-size: cover;
+  right: 0;
+}
+
+.ABPRC-Unit .ABPRC-Control .ABPRC-CommentShow {
+  background-image: url("@{img-url}danmaku.png?");
+  background-size: cover;
+  top: 0;
+  right: 40px;
+  bottom: 0;
+}
+
+.ABPRC-Unit .ABPRC-Control .progress-bar {
+  position: absolute;
+  overflow: hidden;
+  cursor: default;
+  top: 0;
+  right: 40px;
+  bottom: 0;
+  left: 40px;
+  background: #000;
+  .bar {
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    background: #4399ee;
+    opacity: 0.4;
+    &.dark {
+      background: #09c;
+      opacity: 1;
+    }
+  }
+}
+</style>
---
+++ util.js
@@ -0,0 +1,200 @@
+﻿// import _ from 'lodash'
+import { format_time , getQueryString  } from './filters.js'
+import * as Util from 'src/api/util'
+import Vue from 'vue'
+export const hasClass = (obj, cls) => {
+    var obj_class = obj.className, //获取 class 内容.
+        obj_class_lst = obj_class.split(/\s+/); //通过split空字符将cls转换成数组.
+    var x = 0;
+    for (x in obj_class_lst) {
+        if (obj_class_lst[x] == cls) { //循环数组, 判断是否包含cls
+            return true;
+        }
+    }
+    return false;
+}
+
+export const addClass = (obj, cls) => {
+    var obj_class = obj.className, //获取 class 内容.
+        blank = (obj_class != '') ? ' ' : ''; //判断获取到的 class 是否为空, 如果不为空在前面加个'空格'.
+    var added = obj_class + blank + cls; //组合原来的 class 和需要添加的 class.
+    obj.className = added; //替换原来的 class.
+}
+
+export const removeClass = (obj, cls) => {
+    var obj_class = ' ' + obj.className + ' '; //获取 class 内容, 并在首尾各加一个空格. ex) 'abc    bcd' -> ' abc    bcd '
+    obj_class = obj_class.replace(/(\s+)/gi, ' '); //将多余的空字符替换成一个空格. ex) ' abc    bcd ' -> ' abc bcd '
+    var removed = obj_class.replace(' ' + cls + ' ', ' '); //在原来的 class 替换掉首尾加了空格的 class. ex) ' abc bcd ' -> 'bcd '
+    removed = removed.replace(/(^\s+)|(\s+$)/g, ''); //去掉首尾空格. ex) 'bcd ' -> 'bcd'
+    obj.className = removed; //替换原来的 class.
+}
+
+export const TransferString = (content) => {
+    var string = content;
+    try {
+        string = string.replace(/\r\n/ig, "<br>")
+        string = string.replace(/\n/ig, "<br>");
+        string = string.replace(/\\n/ig, "<br>");
+    } catch (e) {
+        // alert(e.message);  
+    }
+    return string;
+}
+
+export const facebook = () => {
+    var parames = this.params;
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId: 1382678158693829,
+            xfbml: true,
+            version: 'v2.3'
+        });
+    };
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+            return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = '//connect.facebook.net/zh_TW/sdk.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+}
+
+export const sharefacebook = (url, picture, source, title, description) => {
+    var vm = this;
+    var href = url || window.location.href || 'http://rcshow.tv/activity/events/Funny/index.php?from=share';
+    var redirect_uri = href + '&callback';
+    if (conf.token) {
+        redirect_uri = href + '&callback&type=' + conf.type + '&kind=' + conf.kind + '&uid=' + conf.uid + '&singerUid=' + conf.suid + '&token=' + conf.token + '&nstat=' + conf.nstat;
+    }
+    var link = 'http://rcshow.tv/appNew/share/fb.php?href=' + encodeURIComponent(href) + '&redirect_uri=' + encodeURIComponent(redirect_uri);
+    return new Promise(function(resolve,reject){
+        FB.ui({
+            method: 'share',
+            href: href,
+            layout: 'box_count',
+            mobile_iframe: true,
+            picture: picture || require('assets/img/550-300.jpg'),
+            source: picture || source,
+            title: title,
+            description: description
+            // caption: caption,
+            // title: title
+        }, function (response) {
+            if (response !== undefined) {
+                rca('app', 'Funny', 'share', '拎北的N種《搞笑短片》', link, function () {
+                    resolve('分享成功');
+                });
+            } else {
+                reject('分享失敗');
+            }
+        })
+    })
+   
+}
+
+export const alertDialogCommpent = (dialogcompont,data) => {
+    if (!dialogcompont) return false;
+    var dialogComponts = new Vue(dialogcompont)
+    dialogComponts.dialogData = data
+    var component = dialogComponts.$mount()
+    document.getElementsByTagName('body')[0].appendChild(component.$el)
+    //document.getElementById('pageDialog').appendChild(component.$el)
+}
+
+
+export const rca = (category, cvar, value, label, link, callback) => {
+    var argsObj = {
+        utc: category,
+        utv: cvar,
+        utt: value,
+        utl: label || '',
+        dl: link || '',
+        vendor: getQueryString('vendor')
+    };
+    argsObj.z = new Date().getTime();
+
+    var argsArr = [];
+    for (var key in argsObj) {
+        argsArr.push(key + '=' + encodeURIComponent(argsObj[key]));
+    }
+
+    var url = 'https://rcshow.tv/data_img.php?' + argsArr.join('&');
+
+    var img = new Image();
+    img.onload = img.onerror = function() {
+        if (typeof callback == 'function') {
+            callback();
+        }
+    };
+    img.src = url;
+}
+
+
+//新增浏览器平台正则对象返回
+export const browserPlatform = function() {
+    var browser = {
+        versions: function () {
+            var u = navigator.userAgent.toLowerCase(),
+                app = navigator.appVersion;
+            return {
+                trident: u.indexOf('trident') > -1, //IE内核
+                presto: u.indexOf('presto') > -1, //opera内核
+                webKit: u.indexOf('applewebkit') > -1, //苹果、谷歌内核
+                gecko: u.indexOf('gecko') > -1 && u.indexOf('khtml') == -1, //火狐内核
+                mobile: !!u.match(/AppleWebKit.*Mobile.*/ig), //是否为移动终端
+                ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/ig), //ios终端
+                android: u.match(/android/i) || u.indexOf('adr') > -1, //android终端
+                iPhone: u.indexOf('iphone') > -1, //是否为iPhone或者QQHD浏览器
+                iPad: u.indexOf('ipad') > -1, //是否iPad
+                webApp: u.indexOf('safari') == -1, //是否web应该程序，没有头部与底部
+                weixin: u.match(/MicroMessenger/i) == "micromessenger", //是否微信 （2015-01-22新增）
+                qq: u.match(/\sQQ/ig) == " qq" //是否QQ
+            };
+        }(),
+        language: (navigator.browserLanguage || navigator.language).toLowerCase()
+    }
+    return browser.versions
+}
+
+export const  download =  function() {
+    var browserPlatform = Util.browserPlatform()
+    var img = new Image();
+    img.src = 'https://rcshow.tv/apps/download/download.php?source=RCPUBG';
+    if (browserPlatform.ios || browserPlatform.iPhone || browserPlatform.iPad) {
+        location.href = 'https://rcshow.tv/apps/download/getApp.php?type=ios&source=RCPUBG'
+    } else if (browserPlatform.mobile || browserPlatform.android) {
+        location.href = 'https://rcshow.tv/apps/download/getApp.php?type=android&source=RCPUBG'
+    }
+    else {
+        location.href = 'https://rcshow.tv/apps/download/getApp.php?type=pc&source=RCPUBG'
+    }
+}
+
+export const checkFile = (file,type,size)=>{
+    const typeSL = {
+        video: ['mp4', 'flv', 'avi', 'mov', 'wmv', 'mkv', 'rmvb', 'm4v','3gp'] //常見的視頻格式
+    };
+    const sizeM = size*1024;
+    const _type = file.type;
+    const _size = file.size;
+    const typeM = typeSL[type];
+    if (_size > sizeM){
+        return {
+            msg:`視頻文件大於${size}M,請重新上傳`,
+            code:-1
+        }
+    }
+    if (!typeM || typeM.indexOf(_type) < 0 ){
+        return {
+            masg: `請上傳以下支持的視頻格式：${typeSL.video.join(',')}`,
+            code: -2
+        }
+    }
+    return {
+        msg:'成功',
+        code:0
+    }
+}
